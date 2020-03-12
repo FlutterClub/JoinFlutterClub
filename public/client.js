a9e135e309179eda12d5cb4b485e9975e95d3612
Shivam Goyal <shivamgoyal1899@gmail.com>
2020-03-13 04:20:21 +0530

    Initialize Project

---
+++ public/client.js
@@ -0,0 +1,173 @@
+$('#email').keyup(function () {
+  const email = $('#email')
+  if (email.val()) {
+    if (!validateEmail(email.val())) {
+      email.css({
+        'color': '#f0506e',
+        'border-color': '#f0506e'
+      })
+    } else {
+      email.css({
+        'color': '#32d296',
+        'border-color': '#32d296'
+      })
+    }
+  }
+})
+
+var validateEmail = function (emailValue) {
+  var regex = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/
+  return regex.test(emailValue)
+}
+
+$('#username').keyup(function () {
+  const username = $('#username')
+  if (username.val()) {
+    // https://aashutoshrathi.glitch.me/api/gh/ to increase API calls without 403.
+    const profile = 'https://aashutoshrathi.glitch.me/api/gh/' + username.val()
+    fetch(profile)
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
+          username.css({
+            color: '#f0506e',
+            'border-color': '#f0506e'
+          })
+        } else {
+          username.css({
+            color: '#32d296',
+            'border-color': '#32d296'
+          })
+        }
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  } else {
+    username.css({
+      'color': '#32d296',
+      'border-color': '#32d296'
+    })
+  }
+})
+
+window.getStatus = function (url) {
+  const request = new XMLHttpRequest()
+  request.onreadystatechange = function () {
+    if (request.readyState === 2) {
+      return request.status
+      // this contains the status code
+    }
+  }
+  request.open('GET', url, true)
+  request.send()
+}
+
+$.put = function (url, data, callback, type) {
+  if ($.isFunction(data)) {
+    type = type || callback
+    callback = data
+    data = {}
+  }
+
+  return $.ajax({
+    url: url,
+    type: 'PUT',
+    success: callback,
+    data: data,
+    contentType: type
+  })
+}
+
+$(function () {
+  $('form').submit(function (event) {
+    event.preventDefault()
+    const username = $('#username').val()
+    const email = $('#email').val()
+    fetch(`https://api.github.com/search/users?q=${username}`)
+      .then(res => res.json())
+      .then((out) => {
+        if (out.total_count === 1) {
+          fetch(`/sendmail/${username}/${email}`)
+            .then((res) => {
+              if (res.status === 200) {
+                // eslint-disable-next-line
+                UIkit.notification({
+                  message: '<span class=\'uk-text-small\' uk-icon=\'icon: thumbs-up\'>A verification E-mail has been sent.</span>',
+                  status: 'success',
+                  pos: 'top-center',
+                  timeout: 2000
+                })
+              }
+            })
+            .catch((e) => {
+              console.error(e)
+              // eslint-disable-next-line
+              UIkit.notification({
+                message: '<span class=\'uk-text-small\' uk-icon=\'icon: warning\'>An error occured. Please try again later.</span>',
+                status: 'danger',
+                pos: 'top-center',
+                timeout: 1000
+              })
+            })
+        } else if (out.total_count === 0) {
+          // eslint-disable-next-line
+          UIkit.notification({
+            message: '<span uk-icon=\'icon: warning\'></span> <span class=\'uk-text-small\'>Wrong Username</span>',
+            status: 'danger',
+            pos: 'top-center',
+            timeout: 1500
+          })
+          $('#username').css({
+            color: '#f0506e',
+            'border-color': '#f0506e'
+          })
+          // setTimeout("location.reload(true);", 2000);
+        }
+      }).catch((err) => {
+        console.error(err)
+        // eslint-disable-next-line
+        UIkit.notification({
+          message: '<span class=\'uk-text-small\' uk-icon=\'icon: warning\'>An error occured. Please try again later.</span>',
+          status: 'danger',
+          pos: 'top-center',
+          timeout: 1000
+        })
+      })
+  })
+})
+
+window.showToast = function () {
+  const username = $('#username').val()
+  const email = $('#email').val()
+
+  if (username === '' && email === '') {
+    // eslint-disable-next-line
+    UIkit.notification({
+      message: '<span uk-icon=\'icon: warning\'></span> <span class=\'uk-text-small\'>Email and Username are required fields</span>',
+      status: 'danger',
+      pos: 'top-center',
+      timeout: 2000
+    })
+  } else if (email === '' || username === '') {
+    var empty = 'Email'
+    if (username.length === 0) {
+      empty = 'Username'
+    }
+    // eslint-disable-next-line
+    UIkit.notification({
+      message: `<span uk-icon='icon: warning'></span> <span class='uk-text-small'>${empty} is required field.</span>`,
+      status: 'danger',
+      pos: 'top-center',
+      timeout: 1500
+    })
+  } else {
+    // eslint-disable-next-line
+    UIkit.notification({
+      message: '<div uk-spinner></div> <span class=\'uk-text-small\'>Processing your request.</span>',
+      status: 'success',
+      pos: 'bottom-center',
+      timeout: 2000
+    })
+  }
+}
---
+++ server.js
@@ -0,0 +1,185 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const path = require('path')
+const crypto = require('crypto')
+const { execSync } = require('child_process')
+const bodyParser = require('body-parser')
+const createMail = require('./createmail')
+const urlcrypt = require('url-crypt')(
+  '~{ry*I)44==yU/]9<7DPk!Hj"R#:-/Z7(hTBnlRS=4CXF'
+)
+const sgMail = require('@sendgrid/mail')
+const {
+  glitch,
+  slack,
+  webhookURL,
+  token,
+  selfEmail,
+  codes
+} = require('./constants')
+
+app.use(bodyParser.json())
+sgMail.setApiKey(process.env.SG_TOKEN)
+
+// Auto-update Glitch with GitHub
+app.post('/git', (req, res) => {
+  const hmac = crypto.createHmac('sha1', glitch)
+  const sig = `sha1=${hmac.update(JSON.stringify(req.body)).digest('hex')}`
+  if (
+    req.headers['x-github-event'] === 'push' &&
+    crypto.timingSafeEqual(
+      Buffer.from(sig),
+      Buffer.from(req.headers['x-hub-signature'])
+    )
+  ) {
+    res.sendStatus(200)
+    const commands = [
+      'git fetch origin master',
+      'git reset --hard origin/master',
+      'git pull origin master --force',
+      'npm i',
+      'refresh'
+    ]
+    for (const cmd of commands) {
+      try {
+        const o = execSync(cmd)
+        console.log(o.toString())
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    console.log('> [GIT] Updated with origin/master')
+  } else {
+    console.log('webhook signature incorrect!')
+    return res.sendStatus(403)
+  }
+})
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+// Send the mail to the given email
+app.get('/sendmail/:username/:id', (req, res, next) => {
+  const { username, id } = req.params
+  const base64 = urlcrypt.cryptObj({
+    email: id,
+    username: username
+  })
+
+  // Invite to Slack
+  const slackUrl = `https://slack.com/api/users.admin.invite?token=${slack}&email=${id}`
+  axios.post(slackUrl)
+
+  // Post invitation message on Slack
+  const time = Math.round(new Date().getTime() / 1000)
+  const message = `${username} got invited to Flutter Club organization on GitHub and Slack`
+  const options = {
+    text: 'Welcome to Flutter Club',
+    attachments: [
+      {
+        color: '#36a64f',
+        title: 'Invitation from Flutter Club',
+        title_link: 'https://github.com/orgs/FlutterClub/people',
+        text: message,
+        footer: 'Slack API',
+        ts: time
+      }
+    ]
+  }
+
+  const sendMessage = () => {
+    return new Promise((resolve, reject) => {
+      axios
+        .post(webhookURL, JSON.stringify(options))
+        .then(response => {
+          return resolve('SUCCESS: Sent slack webhook', response.data)
+        })
+        .catch(error => {
+          return reject(new Error('FAILED: Sent slack webhook', error))
+        })
+    })
+  }
+
+  const loop = async () => {
+    for (let i = 0; i < 3; i++) {
+      console.log('retrying sending message ', i)
+      try {
+        const res = await sendMessage()
+        console.log(res)
+        break
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  loop()
+
+  const verificationurl = `https://${req.get('host')}/verify/${base64}`
+
+  const msg = {
+    from: selfEmail,
+    bcc: selfEmail,
+    to: id,
+    subject: 'Invitation to join Flutter Club Team',
+    html: createMail.createMail(username, verificationurl)
+  }
+
+  sgMail.send(msg)
+})
+
+// Verify the email id through the link, and add as member
+app.get('/verify/:base64', (request, response, next) => {
+  const encryptedData = request.params.base64
+  try {
+    const data = urlcrypt.decryptObj(encryptedData)
+    addMember(data)
+      .then(status => {
+        response.status(status)
+        response.redirect('https://github.com/orgs/FlutterClub/teams')
+      })
+      .catch(err => {
+        console.log(err)
+        response.status(400).send('Error occured. Please try again later.')
+        response.end()
+      })
+  } catch (e) {
+    response.status(400).send('Invalid Link.')
+  }
+})
+
+// Add the member as per their email id
+const addMember = data => {
+  const { email, username } = data
+  const regex = /^20\d{7}@thapar?edu$/; // eslint-disable-line
+  const promise = new Promise((resolve, reject) => {
+    let pref = 'outsiders'
+    if (regex.test(email)) {
+      pref = parseInt(email.substring(0, 4)) + 4
+      console.log('Thaparian')
+    }
+    console.log(pref)
+    const url = `https://api.github.com/teams/${codes[pref]}/memberships/${username}?access_token=${token}`
+    console.log(url)
+
+    axios
+      .put(url)
+      .then(res => {
+        console.log(res.data.url)
+        resolve(200)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
+  return promise
+}
+
+const listener = app.listen(3000 || process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
